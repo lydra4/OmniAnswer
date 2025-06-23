f5683f4e1cbc49a39152d6486b0773910783c404
Song Yi <lydra4@gmail.com>
2025-06-23 13:56:55 +0800

    feat: added guardrails and finalized run method

--- src/agents/modality_classifier.py
+++ src/agents/modality_classifier.py
@@ -1,44 +1,47 @@
 import ast
 import logging
-import os
 import re
 from typing import List
 
-import omegaconf
-from agno.agent import Agent
-from agno.models.google import Gemini
-from agno.models.openai import OpenAIChat
-from dotenv import load_dotenv
+import nltk
+from agents.base_agent import BaseAgent
+from guardrails.guard import Guard
+from guardrails.hub import BanList, ToxicLanguage
+from omegaconf import DictConfig
+
+nltk.download("punkt")
 
 
-class ModalityClassifier(Agent):
-    def __init__(self, cfg: omegaconf.DictConfig, logger: logging.Logger) -> None:
-        load_dotenv()
-        self.cfg = cfg
-        self.logger = logger
-        self.llm = (
-            OpenAIChat(
-                id=self.cfg.model,
-                api_key=os.getenv("OPENAI_API_KEY"),
-                temperature=self.cfg.temperature,
-            )
-            if "gpt-" in self.cfg.model.strip().lower()
-            else Gemini(
-                id=self.cfg.model,
-                api_key=os.getenv("GEMINI_API_KEY"),
-                temperature=self.cfg.temperature,
-            )
-        )
-
-        super().__init__(
-            name=cfg.name,
-            model=self.llm,
-            description=cfg.description,
-            instructions=[cfg.system_message],
-            markdown=cfg.markdown,
+class ModalityClassifier(BaseAgent):
+    def __init__(self, cfg: DictConfig, logger: logging.Logger):
+        super().__init__(cfg=cfg, logger=logger)
+        self.guard = Guard().use_many(
+            BanList(
+                banned_words=[
+                    "meth",
+                    "rape",
+                    "murder",
+                    "porn",
+                    "suicide",
+                    "drug",
+                    "sex",
+                    "kill",
+                ],
+                on_fail="refrain",
+            ),
+            ToxicLanguage(
+                threshold=0.5,
+                validation_method="sentence",
+                on_fail="refrain",
+            ),
         )
 
     def run(self, query: str) -> List[str]:
+        result = self.guard.validate(query)
+        if not result.validation_passed:
+            self.logger.warning(f"Rejected query: {result.error}")
+            raise ValueError(f"Query rejected: {result.error}")
+
         response = super().run(query)
         cleaned = re.sub(
             r"^```(?:json|python)?\s*|\s*```$",
