e7e47b3d7e034cbd80e99cab40dd4aef7f6cc0ff
Song Yi <lydra4@gmail.com>
2025-06-24 16:15:46 +0800

    chore: remove langfuse and due to incompatibility

--- src/agents/base_agent.py
+++ src/agents/base_agent.py
@@ -3,12 +3,10 @@ import os
 from abc import ABC, abstractmethod
 from typing import Any
 
-import openlit
 from agno.agent import Agent
 from agno.models.google import Gemini
 from agno.models.openai import OpenAIChat
 from dotenv import load_dotenv
-from langfuse import get_client
 from omegaconf import DictConfig
 
 
@@ -17,23 +15,12 @@ class BaseAgent(Agent, ABC):
         load_dotenv()
         self.cfg = cfg
         self.logger = logger
-        os.environ["LANGFUSE_PUBLIC_KEY"] = os.getenv("LANGFUSE_PUBLIC_KEY")
-        os.environ["LANGFUSE_SECRET_KEY"] = os.getenv("LANGFUSE_SECRET_KEY")
-        os.environ["LANGFUSE_HOST"] = os.getenv("LANGFUSE_HOST")
-
-        self.langfuse = get_client()
-        self.langfuse.auth_check()
-        openlit.init(
-            tracer=self.langfuse._otel_tracer,
-            disable_batch=True,
-        )
 
         model_id = self.cfg.model.strip().lower()
         self.llm = (
             OpenAIChat(
                 id=self.cfg.model,
                 api_key=os.getenv("OPENAI_API_KEY"),
-                temperature=self.cfg.temperature,
             )
             if model_id.startswith("gpt-")
             else Gemini(
