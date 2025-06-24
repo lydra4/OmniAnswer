87cce10a6aed2dae6f1bb8f38332aaa6bdf0ee30
Song Yi <lydra4@gmail.com>
2025-06-24 11:08:24 +0800

    feat: added langfuse

--- src/agents/base_agent.py
+++ src/agents/base_agent.py
@@ -3,12 +3,12 @@ import os
 from abc import ABC, abstractmethod
 from typing import Any
 
+import openlit
 from agno.agent import Agent
 from agno.models.google import Gemini
 from agno.models.openai import OpenAIChat
 from dotenv import load_dotenv
-from langfuse import Langfuse
-from langsmith import Client
+from langfuse import get_client
 from omegaconf import DictConfig
 
 
@@ -17,14 +17,15 @@ class BaseAgent(Agent, ABC):
         load_dotenv()
         self.cfg = cfg
         self.logger = logger
-        self.langfuse = Langfuse(
-            public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
-            secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
-            host=os.getenv("LANGFUSE_HOST"),
-        )
-        self.clent = Client(
-            api_key=os.getenv("LANGSMITH_API_KEY"),
-            api_url=os.getenv("LANGSMITH_ENDPOINT"),
+        os.environ["LANGFUSE_PUBLIC_KEY"] = os.getenv("LANGFUSE_PUBLIC_KEY")
+        os.environ["LANGFUSE_SECRET_KEY"] = os.getenv("LANGFUSE_SECRET_KEY")
+        os.environ["LANGFUSE_HOST"] = os.getenv("LANGFUSE_HOST")
+
+        self.langfuse = get_client()
+        self.langfuse.auth_check()
+        openlit.init(
+            tracer=self.langfuse._otel_tracer,
+            disable_batch=True,
         )
 
         model_id = self.cfg.model.strip().lower()
