2fe41f84af893ac1785ebe13670b45779534da17
Song Yi <lydra4@gmail.com>
2025-06-27 16:50:18 +0800

    feat: added google image search method

--- src/agents/image_agent.py
+++ src/agents/image_agent.py
@@ -1,8 +1,11 @@
 import logging
+import os
 from typing import Any, List
 
 from agents.base_agent import BaseAgent
 from agno.tools.duckduckgo import DuckDuckGoTools
+from dotenv import load_dotenv
+from google_images_search import GoogleImagesSearch
 from omegaconf import DictConfig
 
 
@@ -15,10 +18,24 @@ class ImageAgent(BaseAgent):
                 DuckDuckGoTools(
                     stop_after_tool_call_tools=["duckduckgo_image"],
                     show_result_tools=["duckduckgo_image"],
-                    fixed_max_results=cfg.image_agent.fixed_max_results,
+                    fixed_max_results=cfg.image_agent.num,
                 )
             ]
         super().__init__(cfg=cfg.image_agent, logger=logger, llm=llm, tools=tools)
+
+    def _google_image_search(self, query: str):
+        load_dotenv()
+        gis = GoogleImagesSearch(
+            developer_key=os.getenv("GOOGLE_API_KEY"),
+            custom_search_cx=os.getenv("GOOGLE_CSE_ID"),
+        )
+        _search_params = {
+            "q": query,
+            "num": self.cfg.image_agent.num,
+            "safe": "high",
+            "imgType": "photo",
+        }
+        gis.search(search_params=_search_params)
 
     def run(self, query: str):
         """
