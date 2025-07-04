badb0b889600cb5ec36c9db92b287ee8d19aaa04
Song Yi <lydra4@gmail.com>
2025-07-04 11:27:15 +0800

    chore: change in code due to change in multi modal team class

--- src/main.py
+++ src/main.py
@@ -4,10 +4,7 @@ import os
 import hydra
 import omegaconf
 from agents.multi_modality.modality_agent import ModalityAgent
-from agents.single_modality.image_agent import ImageAgent
 from agents.single_modality.paraphrase_agent import ParaphraseAgent
-from agents.single_modality.text_agent import TextAgent
-from agents.single_modality.video_agent import VideoAgent
 from teams.multi_modal_team import MultiModalTeam
 from utils.general_utils import load_llm, setup_logging
 
@@ -29,16 +26,11 @@ def main(cfg: omegaconf.DictConfig):
     modality_agent = ModalityAgent(cfg=cfg, logger=logger, llm=llm)
     modalities = modality_agent.run(query=query)
 
-    paraphrase_agent = ParaphraseAgent(cfg=cfg.paraphrase_agent, logger=logger, llm=llm)
+    paraphrase_agent = ParaphraseAgent(cfg=cfg, logger=logger, llm=llm)
     paraphrased_outputs = paraphrase_agent.run(query, modalities=modalities)
 
     multimodal_team = MultiModalTeam(
-        text_agent=TextAgent(cfg=cfg, logger=logger, llm=llm),
-        image_agent=ImageAgent(cfg=cfg, logger=logger, llm=llm),
-        video_agent=VideoAgent(cfg=cfg, logger=logger, llm=llm),
-        cfg=cfg.multimodal_team,
-        logger=logger,
-        llm=llm,
+        paraphrased_outputs=paraphrased_outputs, cfg=cfg, logger=logger, llm=llm
     )
     multimodal_team.run(query=query)
 
