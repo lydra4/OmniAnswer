1497410ae9092374dbdc9e4f095a553778b0f3a2
Song Yi <lydra4@gmail.com>
2025-07-17 16:51:37 +0800

    feat: 2nd iteration of image evaluation, added loading of image and evaluation using CLIP

--- src/evaluation/image_evaluation.py
+++ src/evaluation/image_evaluation.py
@@ -1,29 +1,40 @@
 import logging
-import os
+from io import BytesIO
+from typing import Dict
 
-from deepeval.models.llms import GeminiModel, GPTModel
+import requests
+from evaluation.base_evaluation import BaseEvaluation
 from omegaconf import DictConfig
+from PIL import Image
+from transformers import CLIPModel, CLIPProcessor
 
 
-class ImageEvaluation:
+class ImageEvaluation(BaseEvaluation):
     def __init__(
-        self, cfg: DictConfig, logger: logging.Logger, query: str, output: str
+        self,
+        cfg: DictConfig,
+        logger: logging.Logger,
+        query: str,
+        output: Dict[str, str],
     ):
-        """
-        Initializes the evaluation pipeline with configuration and logger.
-
-        Args:
-            cfg (DictConfig): Configuration for the evaluation pipeline.
-            logger (logging.Logger): Logger instance for tracking execution.
-        """
-        self.cfg = cfg
-        self.logger = logger
-        self.query = query
-        self.output = output
-        self.llm = (
-            GPTModel(model=self.cfg.model, api_key=os.getenv("OPENAI_API_KEY"))
-            if self.cfg.model.startswith("gpt-")
-            else GeminiModel(
-                model_name=self.cfg.model, api_key=os.getenv("GEMINI_API_KEY")
-            )
+        super().__init__(cfg=cfg, logger=logger, query=query, output=output)
+        self.model = CLIPModel.from_pretrained(
+            pretrained_model_name_or_path=self.cfg.image_evaluator.pretrained_model_name_or_path
         )
+        self.processor = CLIPProcessor.from_pretrained(
+            pretrained_model_name_or_path=self.cfg.image_evaluator.pretrained_model_name_or_path
+        )
+
+    def _load_image(self) -> Image.Image:
+        response = requests.get(self.output["image"])
+        response.raise_for_status()
+        return Image.open(BytesIO(response.content)).convert("RGB")
+
+    def _evaluate_clip_relevancy(self, image: Image.Image) -> float:
+        inputs = self.processor(
+            text=[self.query], images=image, return_tensors="pt", padding=True
+        )
+        outputs = self.model(**inputs)
+        logits_per_image = outputs.logits_per_image
+        probs = logits_per_image.softmax(dim=1)
+        return round(probs[0][1].item(), 2)
