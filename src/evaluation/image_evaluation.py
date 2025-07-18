216021520f2538825b11d5cebdb68070b973e781
Song Yi <lydra4@gmail.com>
2025-07-18 15:37:49 +0800

    feat: completed clip relevancy and added image coherence

--- src/evaluation/image_evaluation.py
+++ src/evaluation/image_evaluation.py
@@ -3,6 +3,11 @@ from io import BytesIO
 from typing import Dict
 
 import requests
+from deepeval import evaluate
+from deepeval.metrics.multimodal_metrics.image_coherence.image_coherence import (
+    ImageCoherenceMetric,
+)
+from deepeval.test_case.mllm_test_case import MLLMImage, MLLMTestCase
 from evaluation.base_evaluation import BaseEvaluation
 from omegaconf import DictConfig
 from PIL import Image
@@ -25,8 +30,8 @@ class ImageEvaluation(BaseEvaluation):
             pretrained_model_name_or_path=self.cfg.image_evaluator.pretrained_model_name_or_path
         )
 
-    def _load_image(self) -> Image.Image:
-        response = requests.get(self.output["image"])
+    def _load_image(self, url: str) -> Image.Image:
+        response = requests.get(url)
         response.raise_for_status()
         return Image.open(BytesIO(response.content)).convert("RGB")
 
@@ -34,7 +39,14 @@ class ImageEvaluation(BaseEvaluation):
         inputs = self.processor(
             text=[self.query], images=image, return_tensors="pt", padding=True
         )
-        outputs = self.model(**inputs)
-        logits_per_image = outputs.logits_per_image
-        probs = logits_per_image.softmax(dim=1)
-        return round(probs[0][1].item(), 2)
+        output = self.model(**inputs).logits_per_image.item()
+        return round(output, 2)
+
+    def _image_coherence(self, url: str) -> float:
+        metric = ImageCoherenceMetric()
+        test_case = MLLMTestCase(
+            input=[self.query],
+            actual_output=[MLLMImage(url=self.output["image"], local=False)],
+        )
+        evaluate(test_cases=[test_case], metrics=[metric])
+        return round(metric.measure(test_case=test_case), 2)
