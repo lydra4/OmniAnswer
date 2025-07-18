bb8c07ecb63ec44aa4d16b6f856082cb51987ebf
Song Yi <lydra4@gmail.com>
2025-07-18 16:59:13 +0800

    feat: added helper method

--- src/evaluation/base_evaluation.py
+++ src/evaluation/base_evaluation.py
@@ -1,9 +1,11 @@
 import logging
 import os
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Any, Dict
 
+from deepeval.metrics.base_metric import BaseMetric
 from deepeval.models.llms import GeminiModel, GPTModel
+from deepeval.test_case.llm_test_case import LLMTestCase
 from omegaconf import DictConfig
 
 
@@ -26,6 +28,20 @@ class BaseEvaluation(ABC):
                 model_name=self.cfg.model, api_key=os.getenv("GEMINI_API_KEY")
             )
         )
+
+    def _execute_deepeval_metric(
+        self,
+        input_data: str,
+        output_data: Any,
+        metric: BaseMetric,
+        test_case_class=LLMTestCase,
+    ):
+        if hasattr(metric, "model") and metric.model is None:
+            metric.model = self.llm
+
+        test_case = test_case_class(input=input_data, actual_output=output_data)
+        score = metric.measure(test_case=test_case)
+        return round(score, 2)
 
     @abstractmethod
     def evaluate_all(self) -> Dict[str, float]:
