3eaf8da6e5b510a2b9d61c04f5bf612e627942ef
Ong Song Yi <ong_song_yi@aiap.sg>
2025-08-17 13:52:55 +0800

    chore: change import statement due to change in python script and class name

--- src/agents/single_modality/text_agent.py
+++ src/agents/single_modality/text_agent.py
@@ -1,10 +1,9 @@
 from typing import Any, List, Optional
 
 from agents.base_agent import BaseAgent
-from agno.tools.thinking import ThinkingTools
 from agno.utils.log import logger
 from omegaconf import DictConfig
-from tools.serpapi_search import SerpAPISearch
+from tools.text_search import TextSearch
 
 
 class TextAgent(BaseAgent):
@@ -16,8 +15,7 @@ class TextAgent(BaseAgent):
     ) -> None:
         tools = (
             [
-                ThinkingTools(),
-                SerpAPISearch(cfg=cfg),
+                TextSearch(cfg=cfg),
             ]
             if tools is None
             else tools
@@ -27,7 +25,6 @@ class TextAgent(BaseAgent):
 
     def run_query(self, query: str, **kwargs):
         response = super().run(query)
-        print(response)
         url = response.content.strip()
 
         if not url.startswith("http"):
