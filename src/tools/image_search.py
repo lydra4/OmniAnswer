079cb447351c8d2a3ba93d4921034b42033e1205
Song Yi <lydra4@gmail.com>
2025-08-26 05:41:13 +0000

    fix: refactor code for crewai

--- src/tools/image_search.py
+++ src/tools/image_search.py
@@ -1,57 +1,46 @@
+import logging
 import os
-from io import BytesIO
-from typing import Dict, List
+from typing import List
 
-import requests
-from agno.tools import Toolkit
-from agno.utils.log import logger
+from crewai.tools import BaseTool
 from dotenv import load_dotenv
 from google_images_search import GoogleImagesSearch
 from omegaconf import DictConfig
-from PIL import Image
-from PIL.Image import Image as PILImage
 
 
-class ImageSearch(Toolkit):
-    def __init__(self, cfg: DictConfig, **kwargs):
+class ImageSearch(BaseTool):
+    name: str = "Image Search Tool"
+    description: str = (
+        "Searches Google Images and returns image URLs and fetched images."
+    )
+
+    def __init__(self, cfg: DictConfig, logger: logging.Logger, **kwargs):
         self.cfg = cfg
+        self.logger = logger
         super().__init__(
             name=self.cfg.image_agent.tool_name,
             tools=[self._image_search],
             **kwargs,
         )
 
-    def _fetch_image(self, urls: List[str]) -> Dict[str, PILImage]:
-        images: dict = {}
-        for url in urls:
-            try:
-                response = requests.get(url, timeout=10)
-                if response.status_code == 200:
-                    image = Image.open(BytesIO(response.content))
-                    images[url] = image
-            except Exception as e:
-                logger.warning(f"Invalid image data from '{url}': {e}")
-        return images
-
-    def _image_search(self, query: str) -> Dict[str, PILImage]:
-        logger.info(f"Performing image search on '{query}'.")
+    def _run(self, query: str) -> List[str]:
+        self.logger.info(f"Performing image search on '{query}'.")
         try:
             load_dotenv()
             gis = GoogleImagesSearch(
-                developer_key=os.getenv("DEVELOPER_KEY"),
+                developer_key=os.getenv("GEMINI_API_KEY"),
                 custom_search_cx=os.getenv("GOOGLE_CSE_ID"),
             )
             search_params = {
                 "q": query,
-                "num": 5,
+                "num": self.cfg.image_agent.max_results,
                 "fileType": "jpg|webp|png",
             }
             gis.search(search_params=search_params)
             results = [image.url for image in gis.results()]
-            valid_images = self._fetch_image(urls=results)
 
-            return valid_images
+            return results
 
         except Exception as e:
-            logger.warning(f"Error for query: {query}, {e}.")
+            self.logger.warning(f"Error for query: {query}, {e}.")
             return {}
