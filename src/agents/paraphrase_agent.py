6c80b861b08829561ddd9fc8cb22bbc33d1ffe20
Song Yi <lydra4@gmail.com>
2025-09-10 05:10:46 +0000

    feat: completed paraphrase agent

--- src/agents/paraphrase_agent.py
+++ src/agents/paraphrase_agent.py
@@ -1,37 +1,45 @@
+import json
 import logging
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional
 
+from crewai.tools import BaseTool
 from omegaconf import DictConfig
 
-from agents.base_agent import BaseAgent
+from agents.base_agent.base_agent_task import BaseAgentTask
 
 
-class ParaphraseAgent(BaseAgent):
+class ParaphraseAgent(BaseAgentTask):
     def __init__(
         self,
         cfg: DictConfig,
         logger: logging.Logger,
         llm,
-        tools: Optional[List[Any]] = None,
+        output,
+        tools: Optional[List[BaseTool]] = None,
     ) -> None:
         super().__init__(
             cfg=cfg.paraphrase_agent,
             logger=logger,
             llm=llm,
+            output=output,
             tools=tools,
         )
+
+    def _parse_result(self, result: str) -> Dict[str, str]:
+        result_json = result.json
+        parsed_result = json.loads(result_json)
+        return parsed_result["items"]
 
     def run_query(self, query: str, **kwargs) -> Dict[str, str]:
         modalities = kwargs.get("modalities", [])
         if not modalities:
             raise ValueError("Modalities is empty.")
 
-        self._logger.info(
-            f'Running ParaphraseAgent with query: "{query}" and modalities: "{modalities}"'
+        self.logger.info(
+            f'Running ParaphraseAgent for query: "{query}" and modalities: "{modalities}"'
         )
-        result = self._llm.invoke(
-            query=query,
-            modalities=modalities,
-        )
-        self._logger.info(f'Paraphrase results: "{result}"')
-        return result
+        task = super().create_task(query=query, modalities=modalities)
+        result = task.execute_sync()
+        parsed_result = self._parse_result(result=result)
+        self.logger.info(f'Paraphrase results: "{parsed_result}"')
+        return parsed_result
