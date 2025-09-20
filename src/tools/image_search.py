db20586ea2b0c9173f7e5cd80a077d54b61797b9
Song Yi <lydra4@gmail.com>
2025-09-20 05:56:40 +0000

    refactor: change from class to function

--- src/tools/image_search.py
+++ src/tools/image_search.py
@@ -1,22 +1,13 @@
 import os
 from typing import List
 
-from crewai.tools import BaseTool
+from crewai.tools import tool
 from pexelsapi.pexels import Pexels
 
 
-class ImageSearch(BaseTool):
-    name: str = "Image Search"
-    description: str = "Image Search Tool"
-
-    def __init__(self, num_results: int):
-        super().__init__()
-        self.client: Pexels = Pexels(api_key=os.getenv("PEXELS_API_KEY"))
-        self.num_results = num_results
-
-    async def _run(self, query: str) -> List[str]:
-        search_results = self.client.search_photos(
-            query=query, per_page=self.num_results
-        )
-        images_urls = [result["src"]["medium"] for result in search_results["photos"]]
-        return images_urls
+@tool("image_search")
+async def image_search(query: str, num_results: int) -> List[str]:
+    pexels = Pexels(api_key=os.getenv("PEXELS_API_KEY"))
+    search_results = pexels.search_photos(query=query, per_page=num_results)
+    images_urls = [result["src"]["medium"] for result in search_results["photos"]]
+    return images_urls
