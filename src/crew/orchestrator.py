18ca7932768c5aa1d905b5960bb7e648f17ba6bb
Song Yi <lydra4@gmail.com>
2025-09-29 01:19:36 +0000

    refactor: removal of decorators to read in config from hydra

--- src/crew/orchestrator.py
+++ src/crew/orchestrator.py
@@ -3,7 +3,6 @@ import os
 from typing import Dict
 
 from crewai import LLM, Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import TavilySearchTool
 from omegaconf import DictConfig
 
@@ -12,7 +11,6 @@ from tools.image_search import image_search
 from tools.video_search import video_search
 
 
-@CrewBase
 class Orchestrator:
     def __init__(
         self,
@@ -24,7 +22,6 @@ class Orchestrator:
         self.logger = logger
         self.llm = llm
 
-    @agent
     def text_agent(self) -> Agent:
         return Agent(
             llm=self.llm,
@@ -39,7 +36,6 @@ class Orchestrator:
             **self.cfg.text_agent.agent,
         )
 
-    @agent
     def image_agent(self) -> Agent:
         return Agent(
             llm=self.llm,
@@ -47,7 +43,6 @@ class Orchestrator:
             **self.cfg.image_agent.agent,
         )
 
-    @agent
     def video_agent(self) -> Agent:
         return Agent(
             llm=self.llm,
@@ -55,38 +50,34 @@ class Orchestrator:
             **self.cfg.video_agent.agent,
         )
 
-    @task
     def text_task(self) -> Task:
         return Task(
             description="{text_query}",
-            agent=self.text_agent,
+            agent=self.text_agent(),
             expected_output=self.cfg.text_agent.task.expected_output,
             output_pydantic=StringOutput,
         )
 
-    @task
     def image_task(self) -> Task:
         return Task(
-            description="{image_query}",
-            agent=self.image_agent,
+            description="Find {num_results} images for: {image_query}",
+            agent=self.image_agent(),
             expected_output=self.cfg.image_agent.task.expected_output,
             output_pydantic=StringOutput,
         )
 
-    @task
     def video_task(self) -> Task:
         return Task(
-            description="{video_query}",
-            agent=self.video_agent,
+            description="Find {max_results} images for: {video_query}",
+            agent=self.video_agent(),
             expected_output=self.cfg.video_agent.task.expected_output,
             output_pydantic=StringOutput,
         )
 
-    @crew
     def crew(self) -> Crew:
         return Crew(
-            agents=[self.text_agent, self.image_agent, self.video_agent],
-            tasks=[self.text_task, self.image_task, self.video_task],
+            agents=[self.text_agent(), self.image_agent(), self.video_agent()],
+            tasks=[self.text_task(), self.image_task(), self.video_task()],
             process=Process.sequential,
             verbose=True,
         )
@@ -97,7 +88,16 @@ class Orchestrator:
             inputs={
                 "text_query": paraphrase_queries.get("text", ""),
                 "image_query": paraphrase_queries.get("image", ""),
+                "num_results": self.cfg.image_agent.num_results,
                 "video_query": paraphrase_queries.get("video", ""),
+                "max_results": self.cfg.video_agent.max_results,
             }
         )
-        print(f"Results: {results}")
+        outout = {
+            mode: result
+            for mode, result in zip(
+                paraphrase_queries.keys(),
+                results,
+            )
+        }
+        print(outout)
