b38dff6252bc42f96505146a8c596cd56d3d6ac3
Song Yi <lydra4@gmail.com>
2025-10-08 11:43:03 +0000

    feat: added method to load in results dictionary

--- src/evaluation/evaluation_pipeline.py
+++ src/evaluation/evaluation_pipeline.py
@@ -1,4 +1,6 @@
+import json
 import logging
+import os
 from typing import Dict
 
 import torch
@@ -12,12 +14,11 @@ class EvaluationPipeline:
         self,
         cfg: DictConfig,
         logger: logging.Logger,
-        result_dict: Dict[str, str],
     ) -> None:
         self.cfg = cfg
         self.logger = logger
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.result_dict = result_dict
+        self.result_dict = self._load_results_dict(path=self.cfg.output_path)
 
         if "text" in self.result_dict.keys():
             self.text_metric = BERTScore(
@@ -32,3 +33,12 @@ class EvaluationPipeline:
 
         if "video" in self.result_dict.keys():
             pass
+
+    def _load_results_dict(self, path: str) -> Dict[str, str]:
+        result_dict_path = os.path.join(path, "evaluation_dict.json")
+        if os.path.exists(result_dict_path):
+            try:
+                with open(result_dict_path, "r", encoding="utf-8") as f:
+                    return json.load(f)
+            except Exception as e:
+                self.logger.error(f"Error occured: {e}.")
