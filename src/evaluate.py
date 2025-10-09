67b883f8ba0310ad156ef7d389ff6c848850e5fc
Song Yi <lydra4@gmail.com>
2025-10-09 04:43:18 +0000

    refactor: refactor entry script due to change ine evaluation pipeline

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,17 +1,16 @@
-import json
 import logging
 import os
 
 import hydra
 from omegaconf import DictConfig
-from tqdm import tqdm
 
-from evaluation.image_evaluation import ImageEvaluation
-from evaluation.text_evaluation import TextEvaluation
+from evaluation.evaluation_pipeline import EvaluationPipeline
 from utils.general_utils import setup_logging
 
 
-@hydra.main(version_base=None, config_path="../config", config_name="evaluation.yaml")
+@hydra.main(
+    version_base=None, config_path="../config/evaluation", config_name="evaluation.yaml"
+)
 def main(cfg: DictConfig):
     logger = logging.getLogger(__name__)
     setup_logging(
@@ -21,23 +20,8 @@ def main(cfg: DictConfig):
     )
     logger.info("Setting up logging configuration")
 
-    with open(
-        cfg.output_path,
-        encoding="utf-8",
-    ) as f:
-        multimodal_output = json.load(f)
-
-    text_evaluation = TextEvaluation(cfg=cfg, logger=logger)
-    image_evaluation = ImageEvaluation(cfg=cfg, logger=logger)
-
-    for output_data in tqdm(multimodal_output):
-        query = output_data["original_query"]
-        modalities = list(output_data["results"].keys())
-
-        if "text" in modalities:
-            text_evaluation.evaluate_all(query=query, output_data=output_data)
-        if "image" in modalities:
-            image_evaluation.evaluate_all(query=query, output_data=output_data)
+    evaluation_pipeline = EvaluationPipeline(cfg=cfg, logger=logger)
+    evaluation_pipeline.evaluate()
 
 
 if __name__ == "__main__":
