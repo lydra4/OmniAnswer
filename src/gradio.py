e46a45829ca215884128f877e12b070165a5cdc2
Song Yi <lydra4@gmail.com>
2025-11-17 07:40:01 +0000

    feat: gradio app entry script

---
+++ src/gradio.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+import hydra
+
+from frontend.gradio_app import GradioApp
+from utils.general_utils import setup_logging
+
+
+@hydra.main(version_base=None, config_path="../config", config_name="pipeline.yaml")
+def main(cfg):
+    logger = logging.getLogger(__name__)
+    logger.info("Setting up logging configuration.")
+    setup_logging(
+        logging_config_path=os.path.join(
+            hydra.utils.get_original_cwd(), "config", "logging.yaml"
+        )
+    )
+
+    gradio_app = GradioApp(
+        cfg=cfg,
+        logger=logger,
+    )
+
+
+if __name__ == "__main__":
+    main()
